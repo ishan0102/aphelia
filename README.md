aa2ed298351e6a5c7ac026ad70d3ebc44b838712
Ishan Shah <ishan0102@utexas.edu>
2023-04-21 03:01:50 -0500

    Use cogs to organize commands

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # apheleia
 My personal assistant
+
+## TODO:
+- [ ] Scrape https://papers.labml.ai/papers/weekly/ for new papers periodically
+- [ ] Add a !remindme function that lets me add reminders
+- [ ] Remind me of events 10 minutes before they start
--- src/bot.py
+++ src/bot.py
@@ -1,12 +1,11 @@
-import io
 import os
-import re
 import sys
-
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-from rsrch import upload
+
+# Import the custom cog module
+from cogs.rsrch_cog import RsrchCog
 
 load_dotenv()
 
@@ -15,7 +14,7 @@ intents.messages = True
 intents.guilds = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
 
 
 @bot.event
@@ -23,32 +22,7 @@ async def on_ready():
     print(f"We have logged in as {bot.user}")
 
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user or message.channel.name != "rsrch":
-        return
-
-    # Redirect stdout to a StringIO object
-    original_stdout = sys.stdout
-    sys.stdout = captured_stdout = io.StringIO()
-
-    # Use regular expressions to search for URLs in the message content
-    urls = re.findall("(?P<url>https?://[^\s]+)", message.content)
-
-    # Push the URLs to push_papers
-    upload(urls)
-
-    # Reset stdout to the original value
-    sys.stdout = original_stdout
-
-    # Get the captured output and send it to the channel
-    output = captured_stdout.getvalue()
-    if output:
-        await message.channel.send(f"```\n{output}\n```")
-    else:
-        await message.channel.send("No output was captured.")
-
-    await bot.process_commands(message)
-
+# Load the cog
+bot.add_cog(RsrchCog(bot))
 
 bot.run(os.getenv("DISCORD_BOT_TOKEN"))
