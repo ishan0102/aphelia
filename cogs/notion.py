48e8ffb82d27f33e40c2372680247807dab806ec
Ishan Shah <ishan0102@utexas.edu>
2023-06-03 01:11:34 -0500

    Add functions to get random papers daily

--- cogs/notion.py
+++ cogs/notion.py
@@ -1,12 +1,14 @@
 import io
+import os
+import random
 import re
 import sys
 
 import discord
-import rsrch
 from discord import app_commands
 from discord.ext import commands
 from discord.ext.commands import Context
+from rsrch import RsrchClient
 
 from helpers import checks
 
@@ -14,6 +16,7 @@ from helpers import checks
 class NotionCog(commands.Cog, name="notion"):
     def __init__(self, bot):
         self.bot = bot
+        self.rsrch_client = RsrchClient(token=os.getenv("NOTION_TOKEN"), database_id=os.getenv("NOTION_DATABASE_ID"))
 
     @commands.hybrid_command(
         name="upload",
@@ -30,7 +33,15 @@ class NotionCog(commands.Cog, name="notion"):
         urls = re.findall("(?P<url>https?://[^\s]+)", message)
 
         # Push the URLs to push_papers
-        rsrch.upload(urls)
+        try:
+            self.rsrch_client.upload(urls)
+        except ValueError as e:
+            embed = discord.Embed(
+                title="No valid arXiv URLs found",
+                color=0xE02B2B,
+            )
+            await context.send(embed=embed)
+            return
 
         # Reset stdout to the original value
         sys.stdout = original_stdout
@@ -53,6 +64,27 @@ class NotionCog(commands.Cog, name="notion"):
                 color=0xE02B2B,
             )
 
+        await context.send(embed=embed)
+
+    @commands.command(
+        name="getpaper",
+        description="Retrieve a random research paper from Notion.",
+    )
+    @checks.is_owner()
+    async def get_paper(self, context: Context):
+        papers = self.rsrch_client.fetch_table()
+        if not papers:
+            embed = discord.Embed(
+                title="No papers found",
+                color=0xE02B2B,
+            )
+            await context.send(embed=embed)
+            return
+
+        random_paper = random.choice(papers)
+        title, url, date, authors = random_paper
+        description = f"Authors: {authors}\nDate: {date}"
+        embed = discord.Embed(title=f"{title}", url=url, description=description, color=0x9C84EF)
         await context.send(embed=embed)
 
 
--- cogs/recurring.py
+++ cogs/recurring.py
@@ -1,10 +1,12 @@
-import asyncio
+import os
+import random
 
 import aiohttp
 import discord
 from discord import app_commands
 from discord.ext import commands, tasks
 from discord.ext.commands import Context
+from rsrch import RsrchClient
 
 from helpers import checks
 
@@ -14,6 +16,7 @@ class RecurringCog(commands.Cog, name="recurring"):
         self.bot = bot
         self.notification_channel = None
         self.hn = False
+        self.rsrch_client = RsrchClient(token=os.getenv("NOTION_TOKEN"), database_id=os.getenv("NOTION_DATABASE_ID"))
 
     @commands.command(
         name="setnotifs",
@@ -50,7 +53,6 @@ class RecurringCog(commands.Cog, name="recurring"):
                             url = story_data.get("url")
                             discussion = "https://news.ycombinator.com/item?id=" + str(story)
                             data.append(f"{i}. [{title}]({url}) ({score}, [discussion]({discussion}))")
-                    # embed.description(name="", value="\n".join(data), inline=False)
                     embed = discord.Embed(title="Hacker News Top Stories", description="\n".join(data), color=0x9C84EF)
                     await channel.send(embed=embed)
                 else:
@@ -95,6 +97,54 @@ class RecurringCog(commands.Cog, name="recurring"):
 
         embed = discord.Embed(
             title=f"Hacker News notifications `{enable_choice}d`",
+            color=0x9C84EF,
+        )
+        await context.send(embed=embed)
+
+    @tasks.loop(hours=24.0)
+    async def send_random_paper(self, channel):
+        papers = self.rsrch_client.fetch_table()
+        if not papers:
+            return
+
+        random_paper = random.choice(papers)
+        title, url, date, authors = random_paper
+        description = f"Authors: {authors}\nDate: {date}"
+        embed = discord.Embed(title=f"Random paper to read: {title}", url=url, description=description, color=0x9C84EF)
+        await channel.send(embed=embed)
+
+    @commands.hybrid_command(
+        name="paper",
+        description="Enable or disable daily random paper notifications.",
+    )
+    @checks.is_owner()
+    @app_commands.describe(enable_choice="Your choice (enable/disable)")
+    async def paper(self, context: Context, enable_choice: str):
+        if enable_choice not in ["enable", "disable"]:
+            embed = discord.Embed(
+                title="Invalid choice",
+                description="You must choose between `enable` and `disable`.",
+                color=0xE02B2B,
+            )
+            await context.send(embed=embed)
+            return
+
+        if not self.notification_channel:
+            embed = discord.Embed(
+                title="Notification channel not set",
+                description="You must set a notification channel using the `setnotifs` command.",
+                color=0xE02B2B,
+            )
+            await context.send(embed=embed)
+            return
+
+        if enable_choice == "enable":
+            self.send_random_paper.start(self.notification_channel)
+        elif enable_choice == "disable":
+            self.send_random_paper.cancel()
+
+        embed = discord.Embed(
+            title=f"Daily random paper notifications `{enable_choice}d`",
             color=0x9C84EF,
         )
         await context.send(embed=embed)
